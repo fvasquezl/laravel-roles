02a76f6bdae90d7a2e6a11b18bb81875f64ba8fa
Faustino Vasquez <vasquez.limon@gmail.com>
2025-04-25 16:00:59 -0700

    Roles and Permissions

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Livewire\Products\ProductCreate;
 use App\Livewire\Products\ProductEdit;
 use App\Livewire\Products\ProductIndex;
 use App\Livewire\Products\ProductShow;
+use App\Livewire\Roles\RoleIndex;
 use App\Livewire\Users\UserCreate;
 use App\Livewire\Users\UserEdit;
 use App\Livewire\Users\UserIndex;
@@ -31,6 +32,8 @@ Route::middleware(['auth'])->group(function () {
     Route::get('products/create', ProductCreate::class)->name('products.create');
     Route::get('products/{id}/edit', ProductEdit::class)->name('products.edit');
     Route::get('products/{id}', ProductShow::class)->name('products.show');
+
+    Route::get('roles', RoleIndex::class)->name('roles.index');
 
     Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
     Volt::route('settings/password', 'settings.password')->name('settings.password');
